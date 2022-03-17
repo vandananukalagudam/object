483f331874b6e8a9518ce03373bdcbc8d0b8c54b
vandananukalagudam <vandana.n2001@gmail.com>
2022-03-17 15:48:02 +0530

    object

---
+++ object.js
@@ -0,0 +1,18 @@
+var std={
+    name:"vandana",
+    age:23,
+    rollno:12345,
+    place:{
+        state:"telangana",
+        village:"keesara",
+    }
+}
+console.log(std);
+console.log(std.place);
+console.log(Object.keys(std));
+console.log(Object.values(std));
+console.log(std.name);
+console.log(Object.entries(std)[0]);
+//  var e=Object.entries(std);
+//  console.log(e[0]);
+//  console.log(e[1][1]);
